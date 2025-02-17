3dad6231a9641670dccb20906acfe462fbe39023
Om Harsh <oharsh753@gmail.com>
2025-02-17 17:01:03 +0530

    added model folder containing model, ipynb file and dataset

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import util
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "https://bhp-sigma.vercel.app"}})
+CORS(app,resources={r"/*": {"origins": "http://localhost:5173"}})
 
 @app.route('/')
 def home():
